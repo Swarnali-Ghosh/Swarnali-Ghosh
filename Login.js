c93798beb78e492dd2951e3b79d6fe2bd61025cb
Swarnali Ghosh <95978425+Swarnali-Ghosh@users.noreply.github.com>
2022-07-06 08:02:50 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,1 +1,29 @@
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom';
+import './Login.css';
 
+const Login = () => {
+    return (
+                <Fragment>
+                    <div className='login-page' >
+                        <form className='loginForm'>
+                            <h1 className='logintp'>Login</h1>
+                            <h4 className='logintpo'>welcome back please login to your account</h4>
+                            <input className='inputsign' type="email" placeholder='Email' required/>
+
+                            <input className='inputsign' type="password" placeholder="Password" required/>
+
+                            <Link className='forgotpassword' to="/password/forgot"><b>Forget Password ?</b></Link>
+                            <input type="submit" value="Login" className="loginBtn" />
+                            <div className='asign'>
+                                <h4>don't have an account?</h4> <h4 className='signupfm'>
+                                    <Link to='/signup' className='signupfm'> Signup </Link>
+                                </h4>
+                            </div>
+                        </form>
+                    </div>
+                </Fragment>
+
+    )
+}
+export default Login
